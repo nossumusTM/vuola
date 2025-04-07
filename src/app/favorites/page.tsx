7f563c3e63ebfac838a3028a18f0158e7f1b9be8
LVN <Elvin@Gunays-MacBook-Air.local>
2025-04-07 20:30:06 +0400

    fix

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,7 +1,7 @@
 
 import EmptyState from "@/app/components/EmptyState";
 import ClientOnly from "@/app/components/ClientOnly";
-
+import getCurrentUser from "../actions/getCurrentUser";
  "@/app/actions/getCurrentUser";
 import getFavoriteListings from "@/app/actions/getFavoriteListings";
 
