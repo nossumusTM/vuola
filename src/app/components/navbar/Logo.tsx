17c5cd7745e2ef98e34ea5774ad04454fe58f49f
LVN <Elvin@Gunays-MacBook-Air.local>
2025-04-17 22:18:53 +0400

    General update

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -11,7 +11,7 @@ const Logo = () => {
             <Image
                 onClick={() => router.push('/')}
                 // className="hidden md:block cursor-pointer"
-                className="block cursor-pointer ml-2"
+                className="block cursor-pointer"
                 src="/images/vuoiaggiologo.png"
                 height="150"
                 width="150"
